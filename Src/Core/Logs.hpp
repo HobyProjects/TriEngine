eef2060e0eedfcc1a599262ce5cf7b4ed6cdf313
HobyProjects <hobbyprojects.dev@gmail.com>
2024-10-27 20:03:36 +0530

    Refactor logging macros in Logs.hpp

--- Src/Core/Logs.hpp
+++ Src/Core/Logs.hpp
@@ -21,14 +21,12 @@ namespace TE::Core
 
 }
 
-// Core logger
 #define TE_CORE_TRACE(...)    ::TE::Core::LogSystem::GetCoreLogger()->trace(__VA_ARGS__)
 #define TE_CORE_INFO(...)     ::TE::Core::LogSystem::GetCoreLogger()->info(__VA_ARGS__)
 #define TE_CORE_WARN(...)     ::TE::Core::LogSystem::GetCoreLogger()->warn(__VA_ARGS__)
 #define TE_CORE_ERROR(...)    ::TE::Core::LogSystem::GetCoreLogger()->error(__VA_ARGS__)
 #define TE_CORE_CRITICAL(...) ::TE::Core::LogSystem::GetCoreLogger()->critical(__VA_ARGS__)
 
-// Client logger
 #define TE_TRACE(...)    ::TE::Core::LogSystem::GetClientLogger()->trace(__VA_ARGS__)
 #define TE_INFO(...)     ::TE::Core::LogSystem::GetClientLogger()->info(__VA_ARGS__)
 #define TE_WARN(...)     ::TE::Core::LogSystem::GetClientLogger()->warn(__VA_ARGS__)
